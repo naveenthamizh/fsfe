d603229c8512020cbe4ee36dfacc940e435cd1bc
naveenthamizh <naveenkumar.ksrct@gmail.com>
2026-06-25 21:43:07 +0530

    ws server

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-const http = require("http");
+const express = require("express");
+const server = require("http").createServer();
 
-http.createServer(function(req,res){
+const app = express();
 
-	res.write("FSFE");
-	res.end()
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
 
-}).listen(3000);
+server.on("request", app);
+server.listen(3000, () => {
+  console.log("[Server is listening on port 3000]");
+});
 
-console.log("Server is listening on poer 3000");
+/* Websocket begins */
 
+const WebsocketServer = require("ws").Server;
+
+const wss = new WebsocketServer({ server: server });
+
+wss.on("connection", (conn) => {
+  const numberOfClients = wss.clients.size;
+
+  console.log("Client Connected", numberOfClients);
+
+  wss.broadcast(`Current Visitors ${numberOfClients}`);
+
+  if (conn.readyState === conn.OPEN) {
+    conn.send("[Welcome to my server]");
+  }
+
+  conn.on("close", () => {
+    wss.broadcast(`Current Visitors ${numberOfClients}`);
+    console.log("Client disonnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach((client) => {
+    client.send(data);
+  });
+};
